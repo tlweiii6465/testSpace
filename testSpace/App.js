e7987f0af3ba2a305175c27c0c66b4ef00307938
tlweiii6465 <tlweiii646564@gmail.com>
2018-07-05 10:03:44 +0800

    live chat

--- testSpace/App.js
+++ testSpace/App.js
@@ -1,140 +1,234 @@
-import React, {Component} from 'react';
-import { Platform, AsyncStorage, AppState, StyleSheet,View, Text } from 'react-native';
-import { Grid, Row, Col, Container, Left, Button, Icon, Body, Right,Content } from 'native-base';
-import BlinkView from 'react-native-blink-view';
-import Moment from "moment";
-import AnimateNumber from 'react-native-animate-number'
-class App extends Component{
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
+import {GiftedChat, Actions, Bubble} from 'react-native-gifted-chat';
+import CustomActions from './CustomActions';
+import CustomView from './CustomView';
+
+export default class Example extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isBlinking: true,
-      test: 1,
-      time: new Date().toLocaleString()
+      messages: [],
+      loadEarlier: true,
+      typingText: null,
+      isLoadingEarlier: false,
     };
-}
-componentDidMount() {
-  //for update time in every second
-  this.intervalID = setInterval(
-    () => this.tick(),
-    1000
-  );
-  //for update api data in every min
-  this.intervalAPI = setInterval(
-    () => this.load(),
-    3000
-    
-  ); 
-}
 
-async load(){
-  var old = this.state.test;
-  fetch('https://facebook.github.io/react-native/movies.json', {method: "GET"})
-   .then((response) => response.json())
-   .then((responseData) =>
-   {
-     //set your data here
-     var testing = responseData.movies[4].id
-     
-     let rdnubmer = Math.floor((Math.random() * 100) + 1)
-      this.setState({
-        test:old+rdnubmer,
-      })
-   })
-   .catch((error) => {
-       console.error(error);
-   });
-   
- 
- }
+    this._isMounted = false;
+    this.onSend = this.onSend.bind(this);
+    this.onReceive = this.onReceive.bind(this);
+    this.renderCustomActions = this.renderCustomActions.bind(this);
+    this.renderBubble = this.renderBubble.bind(this);
+    this.renderFooter = this.renderFooter.bind(this);
+    this.onLoadEarlier = this.onLoadEarlier.bind(this);
 
-componentWillUnmount() {
-  clearInterval(this.intervalID,this.intervalAPI);
+    this._isAlright = null;
+  }
 
-}
-tick() {
-  this.setState({
-    time: new Date().toLocaleString()
-  });
-}
-  render() {
-    console.log(this.state.test)
-    ///count time period (AM/PM)
-    var timeP = Moment(this.state.time).format("HH")
-    var timePeroid = (timeP > 11) ? "PM" : "AM";
+  componentWillMount() {
+    this._isMounted = true;
+    this.setState(() => {
+      return {
+        messages: require('./data/messages.js'),
+      };
+    });
+  }
 
-    if(timeP > 12) {
-      timeP -= 12;
-    } else if(timeP == 0) {
-      timeP = "12";
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  onLoadEarlier() {
+    this.setState((previousState) => {
+      return {
+        isLoadingEarlier: true,
+      };
+    });
+
+    setTimeout(() => {
+      if (this._isMounted === true) {
+        this.setState((previousState) => {
+          return {
+            messages: GiftedChat.prepend(previousState.messages, require('./data/old_messages.js')),
+            loadEarlier: false,
+            isLoadingEarlier: false,
+          };
+        });
+      }
+    }, 1000); // simulating network
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, messages),
+      };
+    });
+
+    // for demo purpose
+    this.answerDemo(messages);
+  }
+
+  answerDemo(messages) {
+    if (messages.length > 0) {
+      if ((messages[0].image || messages[0].location) || !this._isAlright) {
+        this.setState((previousState) => {
+          return {
+            typingText: 'Sith lord Darth Vader is typing'
+          };
+        });
+      }
     }
-    //////////
+
+    setTimeout(() => {
+      if (this._isMounted === true) {
+        if (messages.length > 0) {
+          if (messages[0].image) {
+            this.onReceive('Nice picture!');
+          } else if (messages[0].location) {
+            this.onReceive('My favorite place');
+          } else {
+            if (!this._isAlright) {
+              this._isAlright = true;
+
+              this.onReceive("I'm your father.");
+            }
+          }
+        }
+      }
+
+      this.setState((previousState) => {
+        return {
+          typingText: null,
+        };
+      });
+    }, 1000);
+  }
+
+  onReceive(text) {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, {
+          _id: Math.round(Math.random() * 1000000),
+          text: text,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Sith lord Darth Vader',
+            avatar: 'https://i.imgflip.com/1ekvvz.jpg',
+          },
+        }),
+      };
+    });
+  }
+
+  renderCustomActions(props) {
+    if (Platform.OS === 'ios') {
+      return (
+        <CustomActions
+          {...props}
+        />
+      );
+    }
+    const options = {
+      'Image': (props) => {
+        console.log("Image")
+      },
+      'Video': (props) => {
+        console.log("Video")
+      },
+      'Cancel': () => {},
+    };
     return (
-      <Container style={styles.container}>
-        <Content style={styles.borderContent}>
-          <View  style={styles.welcome}>
-            <Text style={styles.welcomeFont}>RM          
-              <AnimateNumber value={this.state.test}
-                countBy={1}
-                timing={(interval, progress) => {
-                // slow start, slow end
-                return interval * (10 - Math.sin(Math.PI*progress) )*10
-              }}/>
-            .00</Text>
-       
-            <Row>
-            <BlinkView blinking={this.state.isBlinking?true:false} delay={600}>
-              <Text style={[styles.center,{color:"#ce1502"}]}>● LIVE </Text>
-            </BlinkView>
-            <Text className="App-clock" style={styles.center}>
-                {Moment(this.state.time).format("hh : mm : ss")} {timePeroid}  AngPow Pool
-            </Text>
-            </Row>
-            
-         
-           
-          </View>
-        </Content>
-        <Content>
-          <Text style={styles.welcome}>QAA TEST</Text>
-        </Content>
-        <Content><Text style={styles.welcome}>QAA TEST</Text></Content>
-      </Container>
-      // <View>
-      //   <Text style={styles.container}>RM888.00</Text>
-      //   
-   
-      // </View>
+      <Actions
+        {...props}
+        options={options}
+      />
+    );
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: '#f0f0f0',
+          }
+        }}
+      />
+    );
+  }
+
+  renderCustomView(props) {
+    return (
+      <CustomView
+        {...props}
+      />
     )
   }
-}
-export default App;
-/////////
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    
-  },
-  welcomeFont: {
-    color:"#ce1502",
-    fontSize: 40, 
-    textAlign: 'center',
-    borderBottomWidth:4,
-    borderBottomColor:"gold"
-  },
-  welcome: {
-    padding: 50,
-  },
-  center:{
-    fontSize: 15, 
-    textAlign: 'center',
-  },
-  borderContent:{
-    backgroundColor: 'white',
-    borderWidth:1,
-    borderColor:'#cecece',
+
+  renderCustomActions(props){
+    return(
+      <CustomActions
+      {...props}
+      />
+    )
   }
+
+
+  renderFooter(props) {
+    if (this.state.typingText) {
+      return (
+        <View style={styles.footerContainer}>
+          <Text style={styles.footerText}>
+            {this.state.typingText}
+          </Text>
+        </View>
+      );
+    }
+    return null;
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={this.onSend}
+        loadEarlier={this.state.loadEarlier}
+        onLoadEarlier={this.onLoadEarlier}
+        isLoadingEarlier={this.state.isLoadingEarlier}
+
+        user={{
+          _id: 1, // sent messages should have same user._id
+          // name: 'Admin',
+          // avatar: 'https://codepo8.github.io/canvas-images-and-pixels/img/horse.png',
+        }}
+
+        renderActions={this.renderCustomActions}
+        renderBubble={this.renderBubble}
+        renderCustomView={this.renderCustomView}
+        renderFooter={this.renderFooter}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  footerContainer: {
+    marginTop: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  footerText: {
+    fontSize: 14,
+    color: '#aaa',
+  },
 });
-/////////////
