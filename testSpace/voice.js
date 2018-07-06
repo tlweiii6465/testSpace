50e02e60b7cd5da98b216bf01d6efbaebc3c3eed
tlweiii6465 <tlweiii646564@gmail.com>
2018-07-06 12:37:25 +0800

    update

---
+++ testSpace/voice.js
@@ -0,0 +1,266 @@
+
+import React, {Component} from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Platform,
+  PermissionsAndroid,
+} from 'react-native';
+import { Grid, Row, Col, Container, Left, Button, Icon, Body, Right } from 'native-base';
+import Sound from 'react-native-sound';
+import {AudioRecorder, AudioUtils} from 'react-native-audio';
+var i = 0;
+class AudioExample extends Component {
+
+    state = {
+      currentTime: "",
+      recording: false,
+      paused: false,
+      stoppedRecording: false,
+      finished: false,
+      audioPath: AudioUtils.DocumentDirectoryPath + '/test.aac',
+      hasPermission: undefined,
+    };
+
+    prepareRecordingPath(audioPath){
+      AudioRecorder.prepareRecordingAtPath(audioPath, {
+        SampleRate: 22050,
+        Channels: 1,
+        AudioQuality: "Low",
+        AudioEncoding: "aac",
+        AudioEncodingBitRate: 32000
+      });
+    }
+
+    componentDidMount() {
+      this._checkPermission().then((hasPermission) => {
+        this.setState({ hasPermission });
+
+        if (!hasPermission) return;
+
+        this.prepareRecordingPath(this.state.audioPath);
+
+        AudioRecorder.onProgress = (data) => {
+          this.setState({currentTime: Math.floor(data.currentTime)});
+        };
+
+        AudioRecorder.onFinished = (data) => {
+          // Android callback comes in the form of a promise instead.
+          if (Platform.OS === 'ios') {
+            this._finishRecording(data.status === "OK", data.audioFileURL);
+          }
+        };
+      });
+    }
+
+    _checkPermission() {
+      if (Platform.OS !== 'android') {
+        return Promise.resolve(true);
+      }
+
+      const rationale = {
+        'title': 'Microphone Permission',
+        'message': 'AudioExample needs access to your microphone so you can record audio.'
+      };
+
+      return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO, rationale)
+        .then((result) => {
+          console.log('Permission result:', result);
+          return (result === true || result === PermissionsAndroid.RESULTS.GRANTED);
+        });
+    }
+
+    _renderButton(title, onPress, active) {
+      var style = (active) ? styles.activeButtonText : styles.buttonText;
+
+      return (
+        <TouchableHighlight style={styles.button} onPress={onPress}>
+          <Text style={style}>
+            {title}
+          </Text>
+        </TouchableHighlight>
+      );
+    }
+
+    _renderPauseButton(onPress, active) {
+      var style = (active) ? styles.activeButtonText : styles.buttonText;
+      var title = this.state.paused ? "RESUME" : "PAUSE";
+      return (
+        <TouchableHighlight style={styles.button} onPress={onPress}>
+          <Text style={style}>
+            {title}
+          </Text>
+        </TouchableHighlight>
+      );
+    }
+
+    async _pause() {
+      if (!this.state.recording) {
+        console.warn('Can\'t pause, not recording!');
+        return;
+      }
+
+      try {
+        const filePath = await AudioRecorder.pauseRecording();
+        this.setState({paused: true});
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async _resume() {
+      if (!this.state.paused) {
+        console.warn('Can\'t resume, not paused!');
+        return;
+      }
+
+      try {
+        await AudioRecorder.resumeRecording();
+        this.setState({paused: false});
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async _stop() {
+      if (!this.state.recording) {
+        console.warn('Can\'t stop, not recording!');
+        return;
+      }
+
+      this.setState({stoppedRecording: true, recording: false, paused: false});
+
+      try {
+        const filePath = await AudioRecorder.stopRecording();
+
+        if (Platform.OS === 'android') {
+          this._finishRecording(true, filePath);
+        }
+        return filePath;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async _play() {
+      if (this.state.recording) {
+        await this._stop();
+      }
+
+      // These timeouts are a hacky workaround for some issues with react-native-sound.
+      // See https://github.com/zmxv/react-native-sound/issues/89.
+      setTimeout(() => {
+        var sound = new Sound(this.state.audioPath, '', (error) => {
+          if (error) {
+            console.log('failed to load the sound', error);
+          }
+        });
+
+        setTimeout(() => {
+          sound.play((success) => {
+            if (success) {
+              console.log('successfully finished playing');
+            } else {
+              console.log('playback failed due to audio decoding errors');
+            }
+          });
+        }, 100);
+      }, 100);
+    }
+
+    async _record() {
+      if (this.state.recording) {
+        console.warn('Already recording!');
+        return;
+      }
+
+      if (!this.state.hasPermission) {
+        console.warn('Can\'t record, no permission granted!');
+        return;
+      }
+
+      if(this.state.stoppedRecording){
+        this.prepareRecordingPath(this.state.audioPath);
+      }
+
+      this.setState({recording: true, paused: false});
+
+      try {
+        const filePath = await AudioRecorder.startRecording();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    _finishRecording(didSucceed, filePath) {
+      this.setState({ finished: didSucceed });
+      console.log("t",filePath)
+      console.log(`Finished recording of duration ${this.state.currentTime} seconds at path: ${filePath}`);
+    }
+
+    render() {
+        
+      return (
+        <View style={styles.container}>
+          <View style={styles.controls}>
+          <Button transparent title="voice" onPress={() => {
+            console.log("Voice of China!");
+            if(i==0){
+              this._record(),this.state.recording;
+              console.log("START!");
+              i=1;
+            }else if(i==1){
+              this._stop()
+              console.log("STOP!");
+              
+              i=0;
+            }
+          }} style={styles.welcome}><Text style={styles.progressText}>RECORD</Text>
+        </Button>
+        <Button onPress={() => {this._play()} } transparent><Text style={styles.progressText}>{this.state.currentTime}s</Text></Button>
+            
+            {/* {this._renderButton("PAUSE", () => {this._pause()} )}
+            {this._renderPauseButton(() => {this.state.paused ? this._resume() : this._pause()})} */}
+            
+          </View>
+        </View>
+      );
+    }
+  }
+
+  var styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#2b608a",
+    },
+    controls: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    progressText: {
+      padding:50,
+      color: "#fff"
+    },
+    button: {
+      padding: 20
+    },
+    disabledButtonText: {
+      color: '#eee'
+    },
+    buttonText: {
+      fontSize: 20,
+      color: "#fff"
+    },
+    activeButtonText: {
+      fontSize: 20,
+      color: "#B81F00"
+    }
+
+  });
+
+export default AudioExample;
