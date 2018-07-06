7de13805769cef8abe9693991304d9f5d3167949
tlweiii6465 <tlweiii646564@gmail.com>
2018-07-06 13:23:56 +0800

    merge

--- testSpace/CustomActions.js
+++ testSpace/CustomActions.js
@@ -1,225 +1,266 @@
 
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
 import {
-  Modal,
+  AppRegistry,
   StyleSheet,
-  TouchableOpacity,
-  View,
-  ViewPropTypes,
   Text,
+  View,
+  TouchableHighlight,
+  Platform,
+  PermissionsAndroid,
 } from 'react-native';
-import CustomView from "./CustomView";
-import CameraRollPicker from 'react-native-camera-roll-picker';
-import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav';
-var ImagePicker = require('react-native-image-picker');
+import { Grid, Row, Col, Container, Left, Button, Icon, Body, Right } from 'native-base';
+import Sound from 'react-native-sound';
 import {AudioRecorder, AudioUtils} from 'react-native-audio';
-let audioPath = AudioUtils.DocumentDirectoryPath + '/test.aac';
-export default class CustomActions extends React.Component {
-  constructor(props) {
-    super(props);
-    this._images = [];
-    this.state = {
-      modalVisible: false,
+var i = 0;
+class AudioExample extends Component {
+
+    state = {
+      currentTime: "",
+      recording: false,
+      paused: false,
+      stoppedRecording: false,
+      finished: false,
+      audioPath: AudioUtils.DocumentDirectoryPath + '/test.aac',
+      hasPermission: undefined,
     };
-    this.onActionsPress = this.onActionsPress.bind(this);
-    this.selectImages = this.selectImages.bind(this);
-  }
 
-  setImages(images) {
-    this._images = images;
-  }
-
-  getImages() {
-    return this._images;
-  }
-
-  setModalVisible(visible = false) {
-    this.setState({modalVisible: visible});
-  }
-
-  onActionsPress() {
-    const options = ['Picture', 'Location', 'Camera', 'Audio', 'Cancel'];
-    const cancelButtonIndex = options.length - 1;
-    this.context.actionSheet().showActionSheetWithOptions({
-      options,
-      cancelButtonIndex,
-    },
-    (buttonIndex) => {
-      switch (buttonIndex) {
-        case 0:
-          this.setModalVisible(true);
-          break;
-        case 1: 
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              this.props.onSend({
-                location: {
-                  latitude: position.coords.latitude,
-                  longitude: position.coords.longitude,                 
-                },               
-              });
-            },
-            (error) => alert(error.message),
-            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-          );
-          break;
-        case 2:
-            ImagePicker.launchCamera({title:"Please click photo"}, (response) => {
-            this.props.onSend([{
-            image: response.uri,
-            }]);
-            this.setImages([]);
-            });
-          break;
-        case 3:
-        AudioRecorder.prepareRecordingAtPath(audioPath, {
-          SampleRate: 22050,
-          Channels: 1,
-          AudioQuality: "Low",
-          AudioEncoding: "aac"
-        });
-        
-          break;
-        default:
-      }
-    });
-  }
-
-  selectImages(images) {
-    this.setImages(images);
-  }
-
-  renderNavBar() {
-    return (
-      <NavBar style={{
-        statusBar: {
-          backgroundColor: '#FFF',
-        },
-        navBar: {
-          backgroundColor: '#FFF',
-        },
-      }}>
-        <NavButton onPress={() => {
-          this.setModalVisible(false);
-        }}>
-          <NavButtonText style={{
-            color: '#000',
-          }}>
-            {'Cancel'}
-          </NavButtonText>
-        </NavButton>
-        <NavTitle style={{
-          color: '#000',
-        }}>
-          {'Camera Roll'}
-        </NavTitle>
-        <NavButton onPress={() => {
-          this.setModalVisible(false);
-
-          const images = this.getImages().map((image) => {
-            return {
-              image: image.uri,
-            };
-          });
-          this.props.onSend(images);
-          this.setImages([]);
-        }}>
-          <NavButtonText style={{
-            color: '#000',
-          }}>
-            {'Send'}
-          </NavButtonText>
-        </NavButton>
-      </NavBar>
-    );
-  }
-
-  renderIcon() {
-    if (this.props.icon) {
-      return this.props.icon();
+    prepareRecordingPath(audioPath){
+      AudioRecorder.prepareRecordingAtPath(audioPath, {
+        SampleRate: 22050,
+        Channels: 1,
+        AudioQuality: "Low",
+        AudioEncoding: "aac",
+        AudioEncodingBitRate: 32000
+      });
     }
-    return (
-      <View
-        style={[styles.wrapper, this.props.wrapperStyle]}
-      >
-        <Text
-          style={[styles.iconText, this.props.iconTextStyle]}
-        >
-          +
-        </Text>
-      </View>
-    );
+
+    componentDidMount() {
+      this._checkPermission().then((hasPermission) => {
+        this.setState({ hasPermission });
+
+        if (!hasPermission) return;
+
+        this.prepareRecordingPath(this.state.audioPath);
+
+        AudioRecorder.onProgress = (data) => {
+          this.setState({currentTime: Math.floor(data.currentTime)});
+        };
+
+        AudioRecorder.onFinished = (data) => {
+          // Android callback comes in the form of a promise instead.
+          if (Platform.OS === 'ios') {
+            this._finishRecording(data.status === "OK", data.audioFileURL);
+          }
+        };
+      });
+    }
+
+    _checkPermission() {
+      if (Platform.OS !== 'android') {
+        return Promise.resolve(true);
+      }
+
+      const rationale = {
+        'title': 'Microphone Permission',
+        'message': 'AudioExample needs access to your microphone so you can record audio.'
+      };
+
+      return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO, rationale)
+        .then((result) => {
+          console.log('Permission result:', result);
+          return (result === true || result === PermissionsAndroid.RESULTS.GRANTED);
+        });
+    }
+
+    _renderButton(title, onPress, active) {
+      var style = (active) ? styles.activeButtonText : styles.buttonText;
+
+      return (
+        <TouchableHighlight style={styles.button} onPress={onPress}>
+          <Text style={style}>
+            {title}
+          </Text>
+        </TouchableHighlight>
+      );
+    }
+
+    _renderPauseButton(onPress, active) {
+      var style = (active) ? styles.activeButtonText : styles.buttonText;
+      var title = this.state.paused ? "RESUME" : "PAUSE";
+      return (
+        <TouchableHighlight style={styles.button} onPress={onPress}>
+          <Text style={style}>
+            {title}
+          </Text>
+        </TouchableHighlight>
+      );
+    }
+
+    async _pause() {
+      if (!this.state.recording) {
+        console.warn('Can\'t pause, not recording!');
+        return;
+      }
+
+      try {
+        const filePath = await AudioRecorder.pauseRecording();
+        this.setState({paused: true});
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async _resume() {
+      if (!this.state.paused) {
+        console.warn('Can\'t resume, not paused!');
+        return;
+      }
+
+      try {
+        await AudioRecorder.resumeRecording();
+        this.setState({paused: false});
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async _stop() {
+      if (!this.state.recording) {
+        console.warn('Can\'t stop, not recording!');
+        return;
+      }
+
+      this.setState({stoppedRecording: true, recording: false, paused: false});
+
+      try {
+        const filePath = await AudioRecorder.stopRecording();
+
+        if (Platform.OS === 'android') {
+          this._finishRecording(true, filePath);
+        }
+        return filePath;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async _play() {
+      if (this.state.recording) {
+        await this._stop();
+      }
+
+      // These timeouts are a hacky workaround for some issues with react-native-sound.
+      // See https://github.com/zmxv/react-native-sound/issues/89.
+      setTimeout(() => {
+        var sound = new Sound(this.state.audioPath, '', (error) => {
+          if (error) {
+            console.log('failed to load the sound', error);
+          }
+        });
+
+        setTimeout(() => {
+          sound.play((success) => {
+            if (success) {
+              console.log('successfully finished playing');
+            } else {
+              console.log('playback failed due to audio decoding errors');
+            }
+          });
+        }, 100);
+      }, 100);
+    }
+
+    async _record() {
+      if (this.state.recording) {
+        console.warn('Already recording!');
+        return;
+      }
+
+      if (!this.state.hasPermission) {
+        console.warn('Can\'t record, no permission granted!');
+        return;
+      }
+
+      if(this.state.stoppedRecording){
+        this.prepareRecordingPath(this.state.audioPath);
+      }
+
+      this.setState({recording: true, paused: false});
+
+      try {
+        const filePath = await AudioRecorder.startRecording();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    _finishRecording(didSucceed, filePath) {
+      this.setState({ finished: didSucceed });
+      console.log("t",filePath)
+      console.log(`Finished recording of duration ${this.state.currentTime} seconds at path: ${filePath}`);
+    }
+
+    render() {
+        
+      return (
+        <View style={styles.container}>
+          <View style={styles.controls}>
+          <Button transparent title="voice" onPress={() => {
+            console.log("Voice of China!");
+            if(i==0){
+              this._record(),this.state.recording;
+              console.log("START!");
+              i=1;
+            }else if(i==1){
+              this._stop()
+              console.log("STOP!");
+              
+              i=0;
+            }
+          }} style={styles.welcome}><Text style={styles.progressText}>RECORD</Text>
+        </Button>
+        <Button onPress={() => {this._play()} } transparent><Text style={styles.progressText}>{this.state.currentTime}s</Text></Button>
+            
+            {/* {this._renderButton("PAUSE", () => {this._pause()} )}
+            {this._renderPauseButton(() => {this.state.paused ? this._resume() : this._pause()})} */}
+            
+          </View>
+        </View>
+      );
+    }
   }
 
-  render() {
-    return (
-      <TouchableOpacity
-        style={[styles.container, this.props.containerStyle]}
-        onPress={this.onActionsPress}
-      >
-        <Modal
-          animationType={'slide'}
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            this.setModalVisible(false);
-          }}
-        >
-          {this.renderNavBar()}
-          <CameraRollPicker
-            maximum={10}
-            imagesPerRow={4}
-            callback={this.selectImages}
-            selected={[]}
-          />
-        </Modal>
-        {this.renderIcon()}
-      </TouchableOpacity>
-    );
-  }
-}
+  var styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#2b608a",
+    },
+    controls: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    progressText: {
+      padding:50,
+      color: "#fff"
+    },
+    button: {
+      padding: 20
+    },
+    disabledButtonText: {
+      color: '#eee'
+    },
+    buttonText: {
+      fontSize: 20,
+      color: "#fff"
+    },
+    activeButtonText: {
+      fontSize: 20,
+      color: "#B81F00"
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    width: 26,
-    height: 26,
-    marginLeft: 10,
-    marginBottom: 10,
-  },
-  wrapper: {
-    borderRadius: 13,
-    borderColor: '#b2b2b2',
-    borderWidth: 2,
-    flex: 1,
-  },
-  iconText: {
-    color: '#b2b2b2',
-    fontWeight: 'bold',
-    fontSize: 16,
-    backgroundColor: 'transparent',
-    textAlign: 'center',
-  },
-});
+  });
 
-CustomActions.contextTypes = {
-  actionSheet: PropTypes.func,
-};
-
-CustomActions.defaultProps = {
-  onSend: () => {},
-  options: {},
-  icon: null,
-  containerStyle: {},
-  wrapperStyle: {},
-  iconTextStyle: {},
-};
-
-CustomActions.propTypes = {
-  onSend: PropTypes.func,
-  options: PropTypes.object,
-  icon: PropTypes.func,
-  containerStyle: ViewPropTypes.style,
-  wrapperStyle: ViewPropTypes.style,
-  iconTextStyle: Text.propTypes.style,
-};
+export default AudioExample;
