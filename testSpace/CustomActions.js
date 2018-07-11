75211214cef89da060d144229c9f95550b9cb393
tlweiii6465 <tlweiii646564@gmail.com>
2018-07-11 14:58:18 +0800

    update

--- testSpace/CustomActions.js
+++ testSpace/CustomActions.js
@@ -2,6 +2,10 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import {
+  TouchableHighlight,
+  Platform,
+  PermissionsAndroid,
+  AppRegistry,
   Modal,
   StyleSheet,
   TouchableOpacity,
@@ -13,22 +17,197 @@ import {
 import CustomView from "./CustomView";
 import CameraRollPicker from 'react-native-camera-roll-picker';
 import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav';
+import Sound from 'react-native-sound';
 import {AudioRecorder, AudioUtils} from 'react-native-audio';
 let audioPath = AudioUtils.DocumentDirectoryPath + '/test.aac';
 var ImagePicker = require('react-native-image-picker');
+import { Grid, Row, Col, Container,Content, Left, Button, Icon, Body, Right } from 'native-base';
 var i = 0;
+
 export default class CustomActions extends React.Component {
+  
   constructor(props) {
     super(props);
     this._images = [];
     this.state = {
+      
       modalVisible: false,
       modalVisible2: false,
+      currentTime: 0,
+      recording: false,
+      paused: false,
+      stoppedRecording: false,
+      finished: false,
+      audioPath: AudioUtils.DocumentDirectoryPath + '/test.aac',
+      hasPermission: undefined,
     };
     this.onActionsPress = this.onActionsPress.bind(this);
     this.selectImages = this.selectImages.bind(this);
   }
 
+  prepareRecordingPath(audioPath){
+    AudioRecorder.prepareRecordingAtPath(audioPath, {
+      SampleRate: 22050,
+      Channels: 1,
+      AudioQuality: "Low",
+      AudioEncoding: "aac",
+      AudioEncodingBitRate: 32000
+    });
+  }
+
+  componentDidMount() {
+    
+    this._checkPermission().then((hasPermission) => {
+      this.setState({ hasPermission });
+
+      if (!hasPermission) return;
+
+      this.prepareRecordingPath(this.state.audioPath);
+
+      AudioRecorder.onProgress = (data) => {
+        this.setState({currentTime: Math.floor(data.currentTime)});
+      };
+
+      AudioRecorder.onFinished = (data) => {
+        // Android callback comes in the form of a promise instead.
+        if (Platform.OS === 'ios') {
+          this._finishRecording(data.status === "OK", data.audioFileURL);
+        }
+      };
+    });
+  }
+
+  _checkPermission() {
+    if (Platform.OS !== 'android') {
+      return Promise.resolve(true);
+    }
+
+    const rationale = {
+      'title': 'Microphone Permission',
+      'message': 'AudioExample needs access to your microphone so you can record audio.'
+    };
+
+    return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO, rationale)
+      .then((result) => {
+        console.log('Permission result:', result);
+        return (result === true || result === PermissionsAndroid.RESULTS.GRANTED);
+      });
+  }
+
+  _renderButton(title, onPress, active) {
+    var style = (active) ? styles.activeButtonText : styles.buttonText;
+
+  }
+
+  _renderPauseButton(onPress, active) {
+    var style = (active) ? styles.activeButtonText : styles.buttonText;
+    var title = this.state.paused ? "RESUME" : "PAUSE";
+  
+  }
+
+
+  async _pause() {
+    if (!this.state.recording) {
+      console.warn('Can\'t pause, not recording!');
+      return;
+    }
+
+    try {
+      const filePath = await AudioRecorder.pauseRecording();
+      this.setState({paused: true});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async _resume() {
+    if (!this.state.paused) {
+      console.warn('Can\'t resume, not paused!');
+      return;
+    }
+
+    try {
+      await AudioRecorder.resumeRecording();
+      this.setState({paused: false});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async _stop() {
+    if (!this.state.recording) {
+      console.warn('Can\'t stop, not recording!');
+      return;
+    }
+
+    this.setState({stoppedRecording: true, recording: false, paused: false});
+
+    try {
+      const filePath = await AudioRecorder.stopRecording();
+
+      if (Platform.OS === 'android') {
+        this._finishRecording(true, filePath);
+      }
+      return filePath;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async _play() {
+    if (this.state.recording) {
+      await this._stop();
+    }
+
+    // These timeouts are a hacky workaround for some issues with react-native-sound.
+    // See https://github.com/zmxv/react-native-sound/issues/89.
+    setTimeout(() => {
+      var sound = new Sound(this.state.audioPath, '', (error) => {
+        if (error) {
+          console.log('failed to load the sound', error);
+        }
+      });
+
+      setTimeout(() => {
+        sound.play((success) => {
+          if (success) {
+            console.log('successfully finished playing');
+          } else {
+            console.log('playback failed due to audio decoding errors');
+          }
+        });
+      }, 100);
+    }, 100);
+  }
+
+  async _record() {
+    if (this.state.recording) {
+      console.warn('Already recording!');
+      return;
+    }
+
+    if (!this.state.hasPermission) {
+      console.warn('Can\'t record, no permission granted!');
+      return;
+    }
+
+    if(this.state.stoppedRecording){
+      this.prepareRecordingPath(this.state.audioPath);
+    }
+
+    this.setState({recording: true, paused: false});
+
+    try {
+      const filePath = await AudioRecorder.startRecording();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  _finishRecording(didSucceed, filePath) {
+    this.setState({ finished: didSucceed});
+    console.log(`Finished recording of duration ${this.state.currentTime} seconds at path: ${filePath}`);
+  }
   setImages(images) {
     this._images = images;
   }
@@ -81,6 +260,7 @@ export default class CustomActions extends React.Component {
           break;
         case 3:
             this.setModalVisible2(true);
+            this._record(),this.state.recording;
           break;
         default:
       }
@@ -125,6 +305,7 @@ export default class CustomActions extends React.Component {
           });
           this.props.onSend(images);
           this.setImages([]);
+   
         }}>
           <NavButtonText style={{
             color: '#000',
@@ -200,24 +381,34 @@ export default class CustomActions extends React.Component {
           justifyContent: 'center',
           alignItems: 'center',
           }} onPress={() => {
+            this._stop()
+            
             console.log("Voice of China!");
-            if(i==0){
-              //this._record(),this.state.recording;
-              console.log("START!");
-              i=1;
-            }else if(i==1){
-              //this._stop()
+            // if(i==0){
+            //   this._record(),this.state.recording;
+            //   console.log("START!");
+            //   i=1;
+            // }else{
+              const voiceMessage = [<View><Text key="key">ICON</Text></View>]
+              
               console.log("STOP!"); 
               this.setModalVisible2(false);     
-              i=0;
-            }
-          
+              //i=0;
+              this.setState({currentTime:0})
+              
+              this.props.onSend({
+                
+                text: voiceMessage,
+                
+                });
+       
+            // }        
         }}>
           <NavButtonText>
             <Image source={require('./asset/imgs/white-speak.png')} />           
           </NavButtonText>
           <Text style={{fontWeight: 'bold',fontSize:15,color:'white'}}>Press to voice message</Text>
-          <Text style={{fontWeight: 'bold',fontSize:30,color:'white'}}>0s</Text>
+          <Text style={{fontWeight: 'bold',fontSize:30,color:'white'}}>{this.state.currentTime}s</Text>
         </NavButton>
        
         </View>
