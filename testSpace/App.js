6dae6e0ab9d207ededd08700e048e4a24ecb0fc4
tlweiii6465 <tlweiii646564@gmail.com>
2018-07-13 16:53:54 +0800

    close

--- testSpace/App.js
+++ testSpace/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Text,View } from 'react-native';
+import { Text,View} from 'react-native';
+import { Container, Content } from "native-base";
 import LottieView from 'lottie-react-native';
 
 export default class BasicExample extends React.Component {
   componentDidMount() {
     this.animation.play();
     // Or set a specific startFrame and endFrame with:
-    // this.animation.play(30, 120);
+    // this.animation.play(1,10);
   }
 
   render() {
     return (
-      <View style={{height:"100%",width:"100%"}}>
+      <Container>
+     
       <LottieView
+      style={{height:"100%",width:"100%"}}
         ref={animation => {
           this.animation = animation;
         }}
-        source={require('./asset/imgs/LineAnimation.json')}
-      /><Text>TEST</Text>
-      </View>
+        source={require('./asset/imgs/jumping_coins.json')}
+        imageAssetsFolder='images'
+      /><Text style={{color:"black",textAlign:'center',marginTop:-200}}>TEST</Text>
+     
+      </Container>
     );
   }
 }
